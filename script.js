99cf6f24a18e370caf1a3e9d667a0c968c0ee67e
Vlad Mikheenko <mkhn.email@gmail.com>
2025-11-29 12:19:19 +0300

    Implement Operations

--- script.js
+++ script.js
@@ -2,11 +2,42 @@
 
 let mainBuffer = '';
 let auxiliaryBuffer = '';
+let currentOperation = '';
+const operations = ['/', '×', '-', '+'];
 const displayOutput = document.querySelector(".display-output");
 
 
 function updateDisplayOutput(output) {
   displayOutput.innerText = output;
+}
+
+
+function obtainResult() {
+  if (!currentOperation) {
+    throw new Error("No current operation.");
+  }
+  switch (currentOperation) {
+    case '/':
+      if (+mainBuffer === 0) {
+        throw new Error("Division by zero.");
+      }
+      return +auxiliaryBuffer / +mainBuffer;
+    case '×':
+      return +auxiliaryBuffer * +mainBuffer;
+    case '-':
+      return +auxiliaryBuffer - +mainBuffer;
+    case '+':
+      return +auxiliaryBuffer + +mainBuffer;
+    default:
+      throw new Error("Unsupported operation has been provided.");
+  }
+}
+
+function startOperation(text) {
+    auxiliaryBuffer = mainBuffer;
+    mainBuffer = '';
+    updateDisplayOutput('0');
+    currentOperation = text;
 }
 
 
@@ -24,6 +55,26 @@ function dispatch(text) {
     mainBuffer = mainBuffer.slice(0, mainBuffer.length - 1);
     console.log(mainBuffer);
     updateDisplayOutput(mainBuffer !== '' ? mainBuffer : '0');
+
+  } else if (operations.includes(text)) {
+    startOperation(text);
+
+  } else if (text === '=') {
+    let result;
+
+    try {
+      result = obtainResult();
+    } catch (error) {
+      return;
+    }
+
+    updateDisplayOutput(result);
+    mainBuffer = String(result);
+    auxiliaryBuffer = '';
+    currentOperation = '';
+
+  } else {
+    throw new Error("Invalid character has been received!");
   }
 }
 
